9370a2f0dbb94c6c2969b1e95d320d35e4abd90c
ftdae <daemonftech@gmail.com>
2025-12-24 21:52:02 +1000

    ppa fee fixing

--- src/calculation/calculates/CoGS/ppa_fees.ts
+++ src/calculation/calculates/CoGS/ppa_fees.ts
@@ -1,26 +1,26 @@
-import {
-  DEFAULT_FIXED_PPA_VALUE,
-  DEFAULT_FLOATING_PPA_VALUE,
-} from "../constant";
-import { IVintage } from "../Revenue/type";
-import { multiplyNumber, roundArray, sumArrays } from "../utils";
+// import {
+//   DEFAULT_FIXED_PPA_VALUE,
+//   DEFAULT_FLOATING_PPA_VALUE,
+// } from "../constant";
+// import { IVintage } from "../Revenue/type";
+// import { multiplyNumber, roundArray, sumArrays } from "../utils";
 
-export function calcPPAFees({
-  fixedPPAValue = DEFAULT_FIXED_PPA_VALUE,
-  floatingPPAValue = DEFAULT_FLOATING_PPA_VALUE,
-  ppaFeesPercentage = 20,
-}: {
-  fixedPPAValue?: number[];
-  floatingPPAValue?: number[];
-  ppaFeesPercentage?: number;
-  modelStartDate?: string;
-  operationStartDate?: string;
-  decommissioningEndDate?: string;
-  vintage?: IVintage;
-}) {
-  const ppaRevenueFee = multiplyNumber(
-    sumArrays(fixedPPAValue, floatingPPAValue),
-    -ppaFeesPercentage / 100
-  );
-  return roundArray(ppaRevenueFee, 20);
-}
+// export function calcPPAFees({
+//   fixedPPAValue = DEFAULT_FIXED_PPA_VALUE,
+//   floatingPPAValue = DEFAULT_FLOATING_PPA_VALUE,
+//   ppaFeesPercentage = 20,
+// }: {
+//   fixedPPAValue?: number[];
+//   floatingPPAValue?: number[];
+//   ppaFeesPercentage?: number;
+//   modelStartDate?: string;
+//   operationStartDate?: string;
+//   decommissioningEndDate?: string;
+//   vintage?: IVintage;
+// }) {
+//   const ppaRevenueFee = multiplyNumber(
+//     sumArrays(fixedPPAValue, floatingPPAValue),
+//     -ppaFeesPercentage / 100
+//   );
+//   return roundArray(ppaRevenueFee, 20);
+// }
