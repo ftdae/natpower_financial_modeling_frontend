252efd406b7b1135bc4f26ddfa1dd38e974115bf
ftdae <daemonftech@gmail.com>
2026-01-09 14:59:43 -0500

    ebit module accuracy checked

--- src/calculation/calculates/ebit.ts
+++ src/calculation/calculates/ebit.ts
@@ -1,27 +1,27 @@
-// import { roundArray, sumArray, sumArrays } from "../calculates/utils";
-// import { DEFAULT_TOTAL_REVENUE } from "./Administrative costs/constant";
-// import {
-//   DEFAULT_TOTAL_ADMIN_COSTS,
-//   DEFAULT_TOTAL_COGS,
-//   DEFAULT_TOTAL_DEPRECIATION,
-// } from "./constant";
+import { roundArray, sumArray, sumArrays } from "../calculates/utils";
+import { DEFAULT_TOTAL_REVENUE } from "./Administrative costs/constant";
+import {
+  DEFAULT_TOTAL_ADMIN_COSTS,
+  DEFAULT_TOTAL_COGS,
+  DEFAULT_TOTAL_DEPRECIATION,
+} from "./constant";
 
-// export function calcEBIT({
-//   totalRevenue = DEFAULT_TOTAL_REVENUE,
-//   totalCoGS = DEFAULT_TOTAL_COGS,
-//   totalAdminCosts = DEFAULT_TOTAL_ADMIN_COSTS,
-//   totalDepreciation = DEFAULT_TOTAL_DEPRECIATION,
-// }: {
-//   totalRevenue?: number[];
-//   totalCoGS?: number[];
-//   totalAdminCosts?: number[];
-//   totalDepreciation?: number[];
-// }) {
-//   return {
-//     ebit: roundArray(
-//       sumArrays(totalRevenue, totalCoGS, totalAdminCosts, totalDepreciation),
-//       20
-//     ),
-//     ebitda: roundArray(sumArrays(totalRevenue, totalCoGS, totalAdminCosts), 20),
-//   };
-// }
+export function calcEBIT({
+  totalRevenue = DEFAULT_TOTAL_REVENUE,
+  totalCoGS = DEFAULT_TOTAL_COGS,
+  totalAdminCosts = DEFAULT_TOTAL_ADMIN_COSTS,
+  totalDepreciation = DEFAULT_TOTAL_DEPRECIATION,
+}: {
+  totalRevenue?: number[];
+  totalCoGS?: number[];
+  totalAdminCosts?: number[];
+  totalDepreciation?: number[];
+}) {
+  return {
+    ebit: roundArray(
+      sumArrays(totalRevenue, totalCoGS, totalAdminCosts, totalDepreciation),
+      20
+    ),
+    ebitda: roundArray(sumArrays(totalRevenue, totalCoGS, totalAdminCosts), 20),
+  };
+}
