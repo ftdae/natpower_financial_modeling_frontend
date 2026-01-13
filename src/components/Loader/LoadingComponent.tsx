07f673494eb525d97214fd3e20ac98aeeace840b
ftdae <daemonftech@gmail.com>
2026-01-14 02:20:24 +0900

    loader updated

--- src/components/Loader/LoadingComponent.tsx
+++ src/components/Loader/LoadingComponent.tsx
@@ -1,28 +1,28 @@
-// import React from "react";
+import React from "react";
 
-// type LoadingComponentProps = {
-//   isLoading: boolean;
-//   loadingText?: string;
-// };
+type LoadingComponentProps = {
+  isLoading: boolean;
+  loadingText?: string;
+};
 
-// const LoadingComponent: React.FC<LoadingComponentProps> = ({
-//   isLoading,
-//   loadingText,
-// }) => {
-//   if (!isLoading) return null;
+const LoadingComponent: React.FC<LoadingComponentProps> = ({
+  isLoading,
+  loadingText,
+}) => {
+  if (!isLoading) return null;
 
-//   return (
-//     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-//       <div className="flex flex-col items-center">
-//         {/* <img src="/loader-tansp.gif" alt="Loading..." className="" /> */}
-//         {loadingText ? (
-//           <p className="text-white text-lg">{loadingText}</p>
-//         ) : (
-//           <p className="text-white text-lg">Loading...</p>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="flex flex-col items-center">
+        {/* <img src="/loader-tansp.gif" alt="Loading..." className="" /> */}
+        {loadingText ? (
+          <p className="text-white text-lg">{loadingText}</p>
+        ) : (
+          <p className="text-white text-lg">Loading...</p>
+        )}
+      </div>
+    </div>
+  );
+};
 
-// export default LoadingComponent;
+export default LoadingComponent;
