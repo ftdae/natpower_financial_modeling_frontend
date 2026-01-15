131b9067671b0580d437ede06e1b6c186cf6a694
ftdae <daemonftech@gmail.com>
2026-01-16 03:10:40 +0900

    operating cf updating

--- src/pages/graph/operating_cashflow.tsx
+++ src/pages/graph/operating_cashflow.tsx
@@ -1,28 +1,28 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-import GroupedTypeSelectorForGraph from "../../components/FinStatements/GroupedTypeSelectorForGraph";
-import { CustomHighchartsGraph } from "./CustomHighchartGraph";
-import { useNetCashFlowSeries } from "../../hooks/graphs/useNetCashFlowSeries";
-import { useOperatingCashFlowSeries } from "../../hooks/graphs/useOperatingCashFlowSeries";
+// import GroupedTypeSelectorForGraph from "../../components/FinStatements/GroupedTypeSelectorForGraph";
+// import { CustomHighchartsGraph } from "./CustomHighchartGraph";
+// import { useNetCashFlowSeries } from "../../hooks/graphs/useNetCashFlowSeries";
+// import { useOperatingCashFlowSeries } from "../../hooks/graphs/useOperatingCashFlowSeries";
 
-export function OperatingCashFlowGraphPage() {
-    const [active, setActive] = useState("quarterly");
+// export function OperatingCashFlowGraphPage() {
+//     const [active, setActive] = useState("quarterly");
 
-    const series = useOperatingCashFlowSeries(active);
+//     const series = useOperatingCashFlowSeries(active);
 
-    return (
-        <div className="flex flex-col w-full  m-auto max-w-[60vw]  rounded-xl p-1 h-full">
-            <div className="flex flex-col gap-4 w-full flex-1 justify-center items-center">
-                <GroupedTypeSelectorForGraph active={active} setActive={setActive} />
+//     return (
+//         <div className="flex flex-col w-full  m-auto max-w-[60vw]  rounded-xl p-1 h-full">
+//             <div className="flex flex-col gap-4 w-full flex-1 justify-center items-center">
+//                 <GroupedTypeSelectorForGraph active={active} setActive={setActive} />
 
-                <div className={` shadow-xl w-full p-1 rounded-xl `}>
-                    <CustomHighchartsGraph
-                        title="Operating CashFlow"
-                        series={series}
-                        yAxis="Operating CashFlow £'000"
-                    />
-                </div>
-            </div>
-        </div>
-    );
-}
+//                 <div className={` shadow-xl w-full p-1 rounded-xl `}>
+//                     <CustomHighchartsGraph
+//                         title="Operating CashFlow"
+//                         series={series}
+//                         yAxis="Operating CashFlow £'000"
+//                     />
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
