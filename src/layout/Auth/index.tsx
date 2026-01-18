a9afc70eeee6d6d2790d3e08a69557ce853b6688
ftdae <daemonftech@gmail.com>
2026-01-18 14:15:33 +0900

    auth page commented

--- src/layout/Auth/index.tsx
+++ src/layout/Auth/index.tsx
@@ -1,26 +1,26 @@
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 
-export default function Index() {
-  return (
-    <>
-      <div className="grid md:grid-cols-2 w-screen h-screen bg-validate-1">
-        <div className="flex items-center justify-center bg-validate-3 s-md:h-28">
-          <img
-            src="/NatPower_Payoff_Bianco.png"
-            alt="NatPowerLogo"
-            className="w-96 h-24"
-          />
-        </div>
-        <div className=" flex justify-center items-center bg-validate-1">
-          <div
-            className={
-              "2xl:w-[526px] lg:w-[450px] md:w-[380px] m-md:w-[385px] md:h-[514px] flex justify-center bg-white text-black dark:bg-white dark:text-black"
-            }
-          >
-            <Outlet />
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
+// export default function Index() {
+//   return (
+//     <>
+//       <div className="grid md:grid-cols-2 w-screen h-screen bg-validate-1">
+//         <div className="flex items-center justify-center bg-validate-3 s-md:h-28">
+//           <img
+//             src="/NatPower_Payoff_Bianco.png"
+//             alt="NatPowerLogo"
+//             className="w-96 h-24"
+//           />
+//         </div>
+//         <div className=" flex justify-center items-center bg-validate-1">
+//           <div
+//             className={
+//               "2xl:w-[526px] lg:w-[450px] md:w-[380px] m-md:w-[385px] md:h-[514px] flex justify-center bg-white text-black dark:bg-white dark:text-black"
+//             }
+//           >
+//             <Outlet />
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
